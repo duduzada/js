807e474b6222161f38015ac5f76b0281a33814cd
duduzada <eduardobarbosaalmeida@gmail.com>
2023-01-04 12:27:03 -0300

    exercicios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,1 +1,21 @@
-alert('Olá!')
+function carregar (){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+  
+    msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos!`
+   
+    if (hora < 6){
+        img.src = 'noite.png'
+    }else if (hora < 12){
+        img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
+    }else if (hora <= 18){
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
+    }else {
+        img.src = 'noite.png'
+    }
+}
