969a68920de30f2005d557228577328373f37708
duduzada <eduardobarbosaalmeida@gmail.com>
2023-01-30 22:54:00 -0300

    exercicio4

---
+++ exercicio4/script.js
@@ -0,0 +1,11 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value == 0) {
+        window.alert('Por favor digite um número') 
+    } else {
+        let n = Number(num.value)
+        for(let c = 1; c <= 10; )
+    }
+   
+}
